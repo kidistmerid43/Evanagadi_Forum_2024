c780b267ba3b0e84206cf5aeb8441efc0932305c
kidistmerid43 <kidistmerid43@gmail.com>
2024-07-14 14:20:14 -0700

    Evanagdi Forum backend deployment

---
+++ Middleware/authMiddleware.js
@@ -0,0 +1,35 @@
+const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+
+async function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader 
+    || !authHeader.startsWith("Bearer ")
+  ) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ message: "No token provided/Authentication: Invalid" });
+  }
+
+
+  const token = authHeader.split(" ")[1];
+  try {
+		const { username, userid } = jwt.verify(
+			token,
+			"secret"
+			// token, process.env.JWT_SECRET
+		)
+		req.user = { username, userid };
+		//  return res
+		// 		.status(StatusCodes.OK)
+		// 		.json({ data });
+
+		next();
+	} catch (error) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ message: "Authentication: Invalid" });
+  }
+}
+module.exports = authMiddleware;
---
+++ Routes/questionRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+// organize and manage routes in a modular and efficient manner
+const questionRouter = express.Router();
+
+const {
+  question,
+  selectquestion,
+  selectsinglequestion,
+  selectansawer,
+} = require("../Controller/questionController");
+questionRouter.post("/askquestion", question);
+questionRouter.get("/selectquestion", selectquestion);
+questionRouter.get("/selectsinglequestion", selectsinglequestion);
+questionRouter.get("/selectansawer", selectansawer);
+
+module.exports = questionRouter;
+
+// const express = require("express");
+// const Router = express.Router();
+// const authMiddleware = require("../Middleware/authMiddleware");
+
+// Router.get("/all-questions",authMiddleware,(req,res)=>{res.send("all questions")})
